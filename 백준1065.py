192b24e55a46d36de539aabd166ff0b11e4991f7
전영서 <20wjsdudtj@gmail.com>
2021-08-02 23:27:25 +0900

    backjoooon

--- 백준1065.py
+++ 백준1065.py
@@ -1,4 +1,12 @@
 a=int(input())
 
 for i in range(1,a+1):
-    pass
+    list_i=list(str(i))
+    for j in range(len(list_i)-1):
+        list.append(list_i[j]-list_i[j+1])
+
+for i in range(len(list)):
+    for j in range(len(list)):
+        if i!=j:
+            break
+    
