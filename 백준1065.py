778a7c1cb1e93be4964ceac776f7296ea0e769fd
Youngseo-Jeon0313 <20wjsdudtj@gmail.com>
2021-12-25 23:51:01 +0900

    .

--- 백준1065.py
+++ 백준1065.py
@@ -5,9 +5,22 @@ for i in range(1,a+1):
     for j in range(len(list_i)-1):
         list.append(list_i[j]-list_i[j+1])
 
+hansu = 0
+
+if list[0]-list[1]==list[1]-
+for n in range(1, num+1) :
+    if n<=99:
+        hansu += 1
+    else:
+        list.append()
+        nums=list(map(int, str(n)))
+        if num[0] -num[1] == num[1] -num[2]:
+            hansu+=1
+
+print(hansu)
+
+
 for i in range(len(list)):
     for j in range(len(list)):
         if i!=j:
             break
-        else:
-            continue
--- 백준4673-1.py
+++ 백준4673-1.py
@@ -7,3 +7,5 @@ for i in range(len(str(n))):
 for i in range(len(str(n))):
     ans+=int(ddlist[i])
 ans+=n
+
+#n이라는 숫자를 str으로 바꾼 후 잘라서 그 앞에 꺼.. 그리고 그 뒤에꺼 더한 다음에 원래 꺼도 더함
