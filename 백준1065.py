42571cda5130ac8dc4bce07fa1e9b5decef9e7d5
Youngseo-Jeon0313 <20wjsdudtj@gmail.com>
2021-12-25 23:54:19 +0900

    .

--- 백준1065.py
+++ 백준1065.py
@@ -5,9 +5,13 @@ for i in range(1,a+1):
     for j in range(len(list_i)-1):
         list.append(list_i[j]-list_i[j+1])
 
+for i in range(len(list_i)):
+    print (i)
 hansu = 0
 
-if list[0]-list[1]==list[1]-
+
+"""
+    
 for n in range(1, num+1) :
     if n<=99:
         hansu += 1
@@ -24,3 +28,5 @@ for i in range(len(list)):
     for j in range(len(list)):
         if i!=j:
             break
+
+        """
