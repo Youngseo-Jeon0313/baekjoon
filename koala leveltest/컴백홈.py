a65e1caef2f5623cd653b05340ef275b67576932
Youngseo-Jeon0313 <20wjsdudtj@gmail.com>
2022-01-10 00:01:00 +0900

    다시ㅠ

--- koala leveltest/컴백홈.py
+++ koala leveltest/컴백홈.py
@@ -10,11 +10,32 @@ C=int(C)
 K=int(K)
 
 for i in range(R):
-    road.append(input().split())
-
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-
+    road.append((input().split()))
 
 print(road)
+making_road=[[0 for col in range(C)] for row in range(R)]
+print(making_road)
+
+for i in range(R):
+    for j in range(C):
+        if (road[i])[j]=='T':
+            making_road[i][j]=0
+        elif j==0 or i==R-1:
+            making_road[i][j]=1
+        else:
+            making_road[i][j]=0
+
+
+for i in range(1,R-1):
+    for j in range(1,C):
+        if road[i][j]=='T':
+            making_road[i][j]=0
+        else:
+            if road[i+1][j]=='T':
+                making_road[i][j]+=making_road[i][j-1]
+            elif road[i][j-1]=='T':
+                making_road[i][j]+=making_road[i+1][j]
+            else:
+                making_road=making_road[i][j-1]+making_road[i+1][j]
+
+print(making_road)
