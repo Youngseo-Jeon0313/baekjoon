a1af2d155780b7567e827722e0f5e49643684a2d
Youngseo-Jeon0313 <20wjsdudtj@gmail.com>
2022-01-23 11:41:38 +0900

    .

--- 1316.py
+++ 1316.py
@@ -9,34 +9,24 @@ D 라는 Dictionary를 만들고,
 
 
 t=int(input())
-sum=t
+sum=0
+
 for i in range(t):
     s=input()
-    D={}
-    s+='1'
+    L=[]
+    flag=True
     if len(s)==2:
         sum+=0
     else:
-        flag=True
-        for j in range(len(s)-1):
-            if flag==False:
-                break
-            if s[j] in list(D.keys()):
-                if D[s[j]]==0 or D[s[j]]==-100: #아니 완전 앞에서 이미 0으로 만들어줌
-                    D[s[j]]=-100
-                    print(1)
-                else: #내 바로 앞에 있는 게 나랑 똑같았을 때
-                    if s[j+1]!=s[j] and j!=len(s)-1:
-                        D[s[j]]=0
-                    D[s[j]]+=1 #다시 더해주기
-                    
-            else: #아직 없네 
-                if s[j+1]!=s[j] and j!=len(s)-1: #근데 내 바로 뒤에 있는 게 나랑 달라
-                        D[s[j]]=0 #일단 a:0으로 만들어주기
+        for i in range(len(s)):
+            if s[i] not in L:
+                L.append(s[i])
+            else:
+                if s[i]==L[-1]:
+                    L.append(s[i])
                 else:
-                    D[s[j]]=1 #일단 a:1로 만들어주기 
-    print(D)
-    if -100 in D.values():
-        sum-=1
-
+                    flag=False
+                    break
+    if flag==True:
+        sum+=1
 print(sum)
