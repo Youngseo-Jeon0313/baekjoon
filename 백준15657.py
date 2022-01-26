5af29078b9b5497f1a9bbba5e34693397b89580d
Youngseo-Jeon0313 <20wjsdudtj@gmail.com>
2022-01-26 14:49:44 +0900

    .

---
+++ 백준15657.py
@@ -0,0 +1,16 @@
+#중복순열로 푸는,,
+
+def go(check, start):
+    if sum(check) == m:
+        ans = [arr[i] for i in range(n) if check[i]]
+        print(' '.join(map(str,ans)))
+        return
+    for i in range(start, n):
+        if check[i] == 0:
+            check[i] = 1
+            go(check, i+1)
+            check[i] = 0
+
+n, m = map(int,input().split())
+arr = sorted(list(map(int,input().split())))
+go([0]*n, 0)
--- 백준2852.py
+++ 백준2852.py
@@ -1,6 +1,48 @@
+from collections import deque
+
+
+
 n=int(input())
 
+one_time=deque() #1번팀이 이기는 시간
+two_time=deque()
+flag=0
 for i in range(n):
     team, time=input().split()
-    m,s=map(int, input().split(':'))
-    
+    m,s=map(int, time.split(':'))
+    if team=='1':
+        flag+=1
+        one_time.append(60*m+s)
+        if len(two_time)>1:
+            two_time.pop()
+        elif len(two_time)==1 and flag==0:
+            x=two_time.pop()
+            two_time.append((60*m+s)-x)
+
+    else: 
+        flag-=1
+        two_time.append(60*m+s)
+        if len(one_time)>1:
+            one_time.pop()
+            continue
+        if len(one_time)==1 and flag==0:
+            x=one_time.pop()
+            one_time.append(60*m+s-x)
+print(one_time)
+print(two_time)
+#끝났을 때 무승부로 끝남
+
+
+'''
+
+
+
+if len(one_time)==1:
+    print(one_time[0])
+if len(two_time)==1:
+    print(two_time[0])
+if len(one_time)>1:
+    print(48*60-one_time[1])
+if len(two_time)>1:
+    print(48*60-two_time[1])
+'''
