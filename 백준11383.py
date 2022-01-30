9d743640cf26038da69790f6c1d18cef39bb7a03
Youngseo-Jeon0313 <20wjsdudtj@gmail.com>
2022-01-30 11:29:22 +0900

    .

--- 백준11383.py
+++ 백준11383.py
@@ -1,4 +1,25 @@
 a,b=map(int, input().split())
 
+picture=[]
 for i in range(a):
-    
+    s1=input()
+    picture.append(s1)
+
+picture_2=[]
+for i in range(a):
+    s2=input()
+    picture_2.append(s2)
+
+flag=True
+for i in range(a):
+    for j in range(b):
+        if picture[i][j]!=picture_2[i][2*j]:
+            flag=False
+            break
+        if picture[i][j]!=picture_2[i][2*j+1]:
+            flag=False
+            break
+if flag==True:
+    print('Eyfa')
+else:
+    print('Not Eyfa')
