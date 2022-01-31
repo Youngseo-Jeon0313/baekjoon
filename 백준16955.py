d002a1776f429a93eb085c2bc476cdc119bb31f9
Youngseo-Jeon0313 <20wjsdudtj@gmail.com>
2022-02-01 02:29:16 +0900

    .

--- 백준16955.py
+++ 백준16955.py
@@ -1,3 +1,6 @@
+import sys
+input=sys.stdin.readline
+
 board=[]
 for i in range(10):
     s=list(input())
@@ -9,49 +12,60 @@ for i in range(10):
         if board[i][j]=='X':
             Apple.append([i,j])
 Apple.sort()
-print(Apple)
 #자 이제 가로, 세로, 대각선 판단
-garo=[]
-sero=[]
-diagonal_right=[]
-diagonal_left=[]
-
+def garo_check(arr,i,j):
+    Garo=''.join(map(str,arr[i][j:j+5]))
+    if Garo=='XXXX.'or Garo=='XXX.X' or Garo=='XX.XX' or Garo== 'X.XXX' or Garo=='.XXXX':
+            return True
+def sero_check(arr,i,j):
+    Sero=[]
+    Sero.append(arr[i][j])
+    Sero.append(arr[i+1][j])
+    Sero.append(arr[i+2][j])
+    Sero.append(arr[i+3][j])
+    Sero.append(arr[i+4][j])
+    Sero=''.join(map(str,Sero))
+    if Sero=='XXXX.' or Sero=='XXX.X' or Sero== 'XX.XX' or Sero=='X.XXX' or Sero== '.XXXX':
+        return True
+def diagonal_left(arr,i,j):
+    Left=[]
+    Left.append(arr[i][j])
+    Left.append(arr[i+1][j-1])
+    Left.append(arr[i+2][j-2])
+    Left.append(arr[i+3][j-3])
+    Left.append(arr[i+4][j-4])
+    Left=''.join(map(str,Left))
+    if Left=='XXXX.' or Left=='XXX.X' or Left=='XX.XX' or Left=='X.XXX' or Left=='.XXXX':
+        return True
+def diagonal_right(arr,i,j):
+    Right=[]
+    Right.append(arr[i][j])
+    Right.append(arr[i+1][j+1])
+    Right.append(arr[i+2][j+2])
+    Right.append(arr[i+3][j+3])
+    Right.append(arr[i+4][j+4])
+    Right=''.join(map(str,Right))
+    if Right=='XXXX.' or Right=='XXX.X' or Right=='XX.XX' or Right=='X.XXX' or Right=='.XXXX':
+        return True
 for i in range(10):
-    for j in range(10):
-        if board[i][j]=='X' and garo[-1]==(10*i-1) :
-            garo.append(10*i+j)
-        elif board[i][j]=='X' and garo[-1]==(10*i-2) and board[i][j-1]=='.':
-            garo.append(10*i+j)
-        if len(garo)==4:
-            if ((garo[-1]==garo[0]+4) or (garo[-1]==garo[0]+3)):
-                print('1')
-                exit()
-            else: garo=[]
-        if board[i][j]=='X' and sero[-1]==(10*i-10) :
-            sero.append(10*i+j)
-        elif board[i][j]=='X' and sero[-1]==(10*i-20) and board[i-1][j]=='.':
-            sero.append(10*i+j)
-        if len(sero)==4:
-            if ((sero[-1]==sero[0]+40) or (sero[-1]==sero[0]+30)):
-                print('1')
-                exit()
-            else: sero=[]
-        if board[i][j]=='X' and diagonal_right[-1]==(10*i-11):
-            diagonal_right.append(10*i+j)
-        elif board[i][j]=='X' and diagonal_right[-1]==(10*i-22) and board[i-1][j-1]:
-            diagonal_right.append(10*i+j)
-        if len(diagonal_right)==4 :
-            if ((diagonal_right[-1]==diagonal_right[0]+44) or (diagonal_right[-1]==sero[0]+33)):
-                print('1')
-                exit()
-            else: diagonal_right=[]
-        if board[i][j]=='X' and diagonal_left[-1]==(10*i-9):
-            diagonal_left.append(10*i+j)
-        elif board[i][j]=='X' and diagonal_left[-1]==(10*i-18) and board[i-1][j+1]=='.':
-            diagonal_left.append(10*i+j)
-        if len(diagonal_left)==4 :
-            if ((diagonal_left[-1]==diagonal_left[0]+44) or (diagonal_left[-1]==sero[0]+33)):
-                print('1')
-                exit()
-            else: diagonal_left=[]
+    for j in range(6):
+        if garo_check(board,i,j):
+            print('1')
+            exit()
+for i in range(6):
+    for j in range(10):        
+        if sero_check(board,i,j):
+            print('1')
+            exit()
+for i in range(6):
+    for j in range(4,10):
+        if diagonal_left(board,i,j):
+            print('1')
+            exit()
+for i in range(6):
+    for j in range(6):
+        if diagonal_right(board,i,j):
+            print('1')
+            exit()
+    
 print('0')
