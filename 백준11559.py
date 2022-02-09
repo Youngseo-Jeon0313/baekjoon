17de79bf4b2c4a81a331b6a28415b0981d8825b1
Youngseo-Jeon0313 <20wjsdudtj@gmail.com>
2022-02-09 19:58:56 +0900

    gridworld

--- 백준11559.py
+++ 백준11559.py
@@ -1,37 +1,42 @@
 from collections import deque
-dx, dy = [1, -1, 0, 0], [0, 0, 1, -1]
+dx, dy= [1,-1,0,0],[0,0,1,-1]
+arr=list(list(input())for _ in range(12))
+Ans=0
 
-n, m = 12, 6
-arr = list(list(input()) for _ in range(n))
-ans = 0
+def check(x,y):
+    if 0<=x<12 and 0<=y<6: return True
+    return False
 
-while 1:
-    puyo_flag = False
-    visit = [[0] * m for _ in range(n)]
-    for i in range(n):
-        for j in range(m):
-            if visit[i][j] == 0 and arr[i][j] != '.':
-                visit[i][j] = 1
-                q = deque([(i, j)])
-                puyo = [(i, j)]
-                while q:
-                    x, y = q.popleft()
-                    for k in range(4):
-                        nx, ny = x + dx[k], y + dy[k]
-                        if 0 <= nx < n and 0 <= ny < m and visit[nx][ny] == 0 and arr[nx][ny] == arr[i][j]:
-                            visit[nx][ny] = 1
-                            q.append((nx, ny))
-                            puyo.append((nx, ny))
-                if len(puyo) >= 4:
-                    puyo_flag = True
-                    for x, y in puyo:
-                        arr[x][y] = '.'
-    if puyo_flag == True:ans += 1
-    else:break
+while True:
+   ans=0
+   check_arr=[[0]*6 for _ in range(12)]
+   for i in range(12):
+       for j in range(6):
+           check_arr[i][j]=1
+           if arr[i][j]!='.':
+               q=deque()
+               pung_q=deque()
+               pung_q.append((i,j))
+               q.append((i,j))
+               while q:
+                  x,y=q.popleft()
+                  for k in range(4):
+                      nx,ny=x+dx[k],y+dy[k]
+                      if check(nx,ny) and arr[nx][ny]==arr[x][y] and check_arr[nx][ny]==0:
+                          pung_q.append((nx,ny))
+                          q.append((nx,ny))
+                          check_arr[nx][ny]=1
+               if len(pung_q)>=4:
+                   ans+=1
+                   for (i,j) in pung_q: arr[i][j]='.'
+   if ans==0: break 
+   Ans+=1
+   
+   for i in range(11):
+       for j in range(11):
+           for k in range(6):
+               if arr[j][k]!='.' and arr[j+1][k]=='.':
+                   arr[j][k], arr[j+1][k]='.', arr[j][k]
 
-    for i in range(n - 1):
-        for x in range(n - 1):
-            for y in range(m):
-                if arr[x][y] != '.' and arr[x + 1][y] == '.':
-                    arr[x][y], arr[x + 1][y] = '.', arr[x][y]
-print(ans)
+   
+print(Ans)
