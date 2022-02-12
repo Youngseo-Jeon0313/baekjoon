e4a71ca60b57b41a1d8f9a1780356d0428dd0e09
Youngseo-Jeon0313 <20wjsdudtj@gmail.com>
2022-02-12 10:15:19 +0900

    dp

--- 백준17626.py
+++ 백준17626.py
@@ -1,15 +1,19 @@
-import sys
-input=sys.stdin.readline
-n=int(input())
-'''
-딱 떨어지게 해야 함 ..!!
-
-'''
-sum=0
-for i in range(224,0,-1):
-    if i**2<=n and n>0:
-        n=n-i**2
-        sum+=1
-    print(i,n)
-
-sys.stdout.write(str(sum)+'\n')
+n = int(input())
+min_sum = 4 #최대는 4라고 증명되어있다, 아래 3중 for문에서 걸리지 않는다면 답은 4
+for a in range(int(n**0.5), int((n//4)**0.5), -1): #가능한 최소한의 범위로 축소해준다
+    if a*a == n:
+        min_sum = 1 #최소의 숫자일 경우
+        break
+    else:
+        temp = n - a*a
+        for b in range(int(temp**0.5), int((temp//3)**0.5), -1): #남은 숫자는 3이니까 3으로 나누어줌
+            if a*a + b*b == n:
+                min_sum = min(min_sum, 2)
+                continue
+            else:
+                temp = n - a*a - b*b
+                for c in range(int(temp**0.5), int((temp//2)**0.5), -1):
+                    if n == a*a + b*b + c*c:
+                        min_sum = min(min_sum, 3)
+                
+print(min_sum)
