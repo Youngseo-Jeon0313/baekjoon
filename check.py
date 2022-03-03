cb5e8f1f57123be4edd9fd431f02a467da2616a4
Youngseo-Jeon0313 <20wjsdudtj@gmail.com>
2022-03-03 12:35:11 +0900

    cb,ascii

--- check.py
+++ check.py
@@ -1,1 +1,7 @@
-print(ord('a'))
+print(ord('a')-96)
+print(ord('c')-96)
+print(ord('i')-96)
+print(ord('n')-96)
+print(ord('t')-96)
+
+print(ord('z')-96)
--- 백준1062.py
+++ 백준1062.py
@@ -1,4 +1,9 @@
 from itertools import combinations as cb
+Alphalist=[i for i in range(1,27)]
 
 
+#1,3,9,14,20
 N,K=map(int,input().split())
+if K<5: print(0)
+elif K==5: #a,c,i,n,t
+else: 
