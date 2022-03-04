f270366e3e26ad52e1243cf6c6485d6a97744f95
Youngseo-Jeon0313 <20wjsdudtj@gmail.com>
2022-03-04 11:48:30 +0900

    완전탐색

--- 백준2992.py
+++ 백준2992.py
@@ -1,32 +1,13 @@
-'''
-일단 구성이 맞는 수들을 다 정렬하고
-그 다음에 가장 작은 거 꺼내오고,
-그 다음에 그 숫자가 나보다 작으면 안 됨
-'''
+import sys
+input=sys.stdin.readline
 
-n=int(input())
-
-#경우의 수는 n!
-L=list(str(n))
-
-
-k=0
-for i in range(1,len(L)):
-    if int(L[i])>int(str(n)[0]):
-        print((L[i]),end='')
-        L.pop(i)
-        L.sort()
-        print(''.join(L))
-        break
-    elif int(L[i])==int(str(n)[0]):
-        if k!=0:
-            print(str(n)[:i],end='')
-            for j in range(i):
-                L.pop(i)
-            L.
-    else:
-        k+=1
-
-
-if k==len(str(n))-1:
-    print(0)
+X=int(input())
+numarr=[0 for _ in range(10)]
+for i in str(X):
+    numarr[int(i)]+=1
+for j in range(X+1,999999):
+    arr=[0 for _ in range(10)]
+    for k in str(j):
+        arr[int(k)]+=1
+    if arr==numarr: print(j); exit();
+print(0)
