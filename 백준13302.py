72defbbb8b7a1c6febdbad4c83ba9f26b93e2c56
20wjsdudtj@gmail.com <20wjsdudtj@gmail.com>
2022-03-16 17:26:31 +0900

    find rules

--- 백준13302.py
+++ 백준13302.py
@@ -3,18 +3,20 @@ arr=[True]*(N+1)
 cant=list(map(int,input().split()))
 for i in cant: arr[i]=False
 coupon=0
-costs=[[0,0]]*N
+costs=[[0,0]]
 print(costs)
 cost=0
+
+max_ticket = (N//5)*2 + (N % 5)//3 + 1
+#쿠폰 쓸지 말지는... 따로 정해야 하나??
 for i in range(1,len(arr)+1):
-    judge=[]
-    if arr[i]==False: judge.append([cost,coupon])
-    else:
-        if coupon > 0: judge.append([cost,coupon-1])
-        judge.append([costs[i-1][0]+10000,coupon])
-        if i>=3:judge.append([costs[i-3][0]+25000,coupon+1])
-        if i>=5:judge.append([costs[i-5][0]+37000,coupon+2])
-    judge.sort(key=lambda x: x[0])
-    print(judge)
-    costs[i][0]=judge[0][0]; cost=judge[0][0]; coupon=judge[0][1]
-    if i==len(arr): print(arr[-1]); break;
+        judge=[]
+        if arr[i]==False: 
+                costs[i].append([costs[i-1][0],costs[i-1][1]])
+        if costs[i-1][1] >= 3: costs[i].append([costs[i-1][0],costs[i-1][1]-3])
+        if i>=1: costs[i].append([costs[i-1][0]+10000,costs[i-1][1]])
+        if i>=3: costs[i].append([costs[i-3][0]+25000,costs[i-3][1]+1])
+        if i>=5:costs[i].append([costs[i-5][0]+37000,costs[i-5][1]+2])
+        if i==len(arr): costs[i]=sorted(costs[i], key=lambda x:x[0]); print(costs[i][0])
+#62000
+print(costs)
--- 백준24678.py
+++ 백준24678.py
@@ -4,21 +4,28 @@ input=sys.stdin.readline
 게임이론
 다들 최선을 다해야 한다..??
 '''
-def game(x,y,z):
-    global flag
-    if (x==0 and y==0) or (x==0 and z==0) or (y==0 and z==0): return flag
-    if (max(x,y,z)==x and y>0 and z>0) or x==0 : x+=1;y-=1;z-=1; game(x,y,z); flag = not flag;
-    elif (max(x,y,z)==y and x>0 and z>0) or y==0: x-=1;y+=1;z-=1; game(x,y,z); flag=not flag;
-    elif (max(x,y,z)==z and y>0 and x>0) or z==0: x-=1; y-=1; z+=1; game(x,y,z); flag=not flag;
-
+def turn(x,y,z,f):
+    flag=False
+    global answer
+    if x>0 and y>0: flag=True; turn(x-1,y-1,z+1,f+1); 
+    elif x>0 and z>0: flag=True; turn(x-1,y+1,z-1,f+1); 
+    elif y>0 and z>0: flag=True; turn(x+1,y-1,z-1,f+1);
+    if flag==False: answer=max(answer,f)
+    return answer
+        
 t=int(input())
 for i in range(t):
-    flag=True
+    answer=0
     x,y,z=map(int,input().split()) 
-    minus=min(x,y,z)
-    if minus>0: 
-        x-=minus; y-=minus; z-=minus;
-        if minus%2:flag=not flag;
-    game(x,y,z)
-    if flag:print('R')
+    if y>0 and z>0:
+        if x%2==1: x=1
+        else: x=2;
+    if x>0 and y>0:
+        if z%2==1: z=1
+        else: z=2
+    if x>0 and z>0:
+        if y%2==1: y=1
+        else: y=2
+    turn(x,y,z,answer)
+    if answer%2==0: print('R')
     else: print('B')
