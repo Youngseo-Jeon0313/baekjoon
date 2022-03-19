1ad49f13cd4e13969ec585c002cb59c6dcc99975
20wjsdudtj@gmail.com <20wjsdudtj@gmail.com>
2022-03-19 18:27:58 +0900

    dijkstra

--- 백준13302.py
+++ 백준13302.py
@@ -2,18 +2,17 @@ N,M=map(int,input().split())
 arr=[True]*(N+1)
 cant=list(map(int,input().split()))
 for i in cant: arr[i]=False
+
 coupon=0
-costs=[]
-max_ticket = (N//5)*2 + (N % 5)//3 + 1
-#쿠폰 쓸지 말지는... 따로 정해야 하나??
+costs=[];coupon=[]
+
 for i in range(1,len(arr)):
-        judge=[]
         if arr[i]==False: 
-                judge[i].append([costs[i-1][0],costs[i-1][1]])
-        if costs[i-1][1] >= 3: judge[i].append([costs[i-1][0],costs[i-1][1]-3])
-        if i>=1: judge[i].append([costs[i-1][0]+10000,costs[i-1][1]])
-        if i>=3: judge[i].append([costs[i-3][0]+25000,costs[i-3][1]+1])
-        if i>=5:judge[i].append([costs[i-5][0]+37000,costs[i-5][1]+2])
-        if i==len(arr)-1: judge[i]=sorted(costs[i], key=lambda x:x[0]); print(costs[i][0])
+                costs[i].append([costs[i-1][0],costs[i-1][1]])
+        if costs[i-1][1] >= 3: costs[i].append([costs[i-1][0],costs[i-1][1]-3])
+        if i>=1: costs[i].append([costs[i-1][0]+10000,costs[i-1][1]])
+        if i>=3: costs[i].append([costs[i-3][0]+25000,costs[i-3][1]+1])
+        if i>=5:costs[i].append([costs[i-5][0]+37000,costs[i-5][1]+2])
+        if i==len(arr)-1: costs[i]=sorted(costs[i], key=lambda x:x[0]); print(costs[i][0])
 #62000
 print(costs)
--- 백준16167.py
+++ 백준16167.py
@@ -6,17 +6,26 @@ def dijkstra(s): #출발하는 곳
         t,x=heappop(hq)
         if cost[x]!=t: continue
         for nx, nt in adj[x]:
-            if cost[nx]>t+nt:
+            if cost[nx]>t+nt :
                 cost[nx]=t+nt
+                road[nx]=road[x]+1
+                heappush(hq, [cost[nx],nx])
+            if cost[nx]==t+nt and road[x]+1<road[nx]: 
+                road[nx]=road[x]+1
                 heappush(hq, [cost[nx],nx])
     return cost
 
 from heapq import *
 n,r=map(int,input().split())
-
 adj=[[] for _ in range(n+1)]
+road=[0]*(n+1)
+road[1]=1
 for i in range(r):
     a,b,c,d,e=map(int,input().split())
     if e<=10: c=c
-    else: c+=(e-10)
+    else: c+=(e-10)*d
     adj[a].append([b,c])
+a=dijkstra(1)
+if a[n]==float('inf'): print('It is not a great way.')
+else: print(a[n], road[n])
+#print(dijkstra(1)[r])
