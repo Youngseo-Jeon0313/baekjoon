ec80b4c8b99f021e4bf6e6a688b38a4c3a7f6500
20wjsdudtj@gmail.com <20wjsdudtj@gmail.com>
2022-03-21 16:49:07 +0900

    floodfill

--- 백준19952.py
+++ 백준19952.py
@@ -4,14 +4,26 @@ t=int(input())
 for _ in range(t):
     H,W,O,F,Xs,Ys,Xe,Ye=map(int,input().split())
     arr=[list(map(int,input().split())) for _ in range(O)]
-    dx,dy=[1,0,-1,0],[0,1,0,-1]
-    check=[[False]*W for _ in range(H)]
-    arr[Xe][Ye]=1
-    check[Xs][Ys]=1
+    road=[[0]*W for _ in range(H)]
+    for j in arr: road[j[0]-1][j[1]-1]=j[2]
+    dx,dy=[-1,0,1,0],[0,-1,0,1]
+    check=[[0]*W for _ in range(H)]
+    check[Xs-1][Ys-1]=1 #시작점
     q=deque()
-    q.append((Xe,Ye))
+    q.append((Xs-1,Ys-1,F))
+    arr=[[0]*W for _ in range(H)]
+    flag=True
     while q:
+        X,Y,f=q.popleft()
+        if X==Xe-1 and Y==Ye-1 and F>=0 : flag=False; break;
+        if f<=0: continue; #힘이 없어요 못가요ㅠㅠ
         for i in range(4):
-            x,y=Xe+dx[i],Ye+dy[i]
-            if 0<=x<H and 0<=y<W:
-                if not check[x][y] and 
+            x,y=X+dx[i],Y+dy[i]
+            if 0<=x<H and 0<=y<W and not check[x][y]:
+                if f>=road[x][y]-road[X][Y]:
+                    check[x][y]=1
+                    q.append([x,y,f-1])
+    if flag==False: print('잘했어!!')
+    else: print('인성 문제있어??')
+
+    
