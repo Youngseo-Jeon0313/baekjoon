355f6d7e3ca97b90ebe1e5e8fed60cd4cc622020
20wjsdudtj@gmail.com <20wjsdudtj@gmail.com>
2022-03-23 00:00:42 +0900

    [[]]*(N+1)과[[] for _ in range(N+1)] 의 차이

--- 백준11952.py
+++ 백준11952.py
@@ -1,17 +1,16 @@
 N,M,K,S=map(int,input().split())
 p,q=map(int,input().split())
 arr=[0]*(N+1)
-road=[[]]*(N+1)
+road=[[] for _ in range(N+1)] #road=[[]]*(N+1) 이거 절대 안 됨 ㅠㅠ
 zombie=[]
-for _ in range(K): zombie.append(int(input()))
+for j in range(K): zombie.append(int(input()))
 for i in range(M):
     a,b=map(int,input().split())
     road[a].append(b)
     road[b].append(a)
 
-print(road)
 def dfs(index,num):
-    if num<=S:
+    if num<S:
         for j in road[index]:
             if not visited[j]:
                 visited[j]=1
@@ -19,9 +18,7 @@ def dfs(index,num):
 
 visited=[0]*(N+1)
 for i in zombie: 
-    visited[i]=1
     dfs(i,0)
-
 print(visited)
 
 #다익스트라로 위험구간 표시 + dfs
