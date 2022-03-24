2d8978967b36c4ab9347ca3c00115315e30e7d55
20wjsdudtj@gmail.com <20wjsdudtj@gmail.com>
2022-03-24 13:07:30 +0900

    dfs 풀 때 주의할 것!!(만약에 A에서 시작하는 bfs와 B에서 시작하는 bfs가 겹칠 경우 그냥 재귀함수를 -not visited[N]: bfs(~~,n+1) 이렇게 하면 중복으로 겹치는 부분이 나올 수 있따!!

--- 백준11952.py
+++ 백준11952.py
@@ -1,22 +1,29 @@
 import sys
+from this import d
 input=sys.stdin.readline
 import heapq
+from collections import deque
 
 N,M,K,S=map(int,input().split())
 P,Q=map(int,input().split())
 road=[[] for _ in range(N+1)] #road=[[]]*(N+1) 이거 절대 안 됨 ㅠㅠ
 zombie=[int(input()) for _ in range(K)]
-for i in range(M):
+for _ in range(M):
     a,b=map(int,input().split())
     road[a].append(b)
     road[b].append(a)
 
+
 def dfs(index,num):
+    deque=deque()
+    deque.append([index,0])
+    while deque:
+        
     if num==S: return #이게 더 메모리 적게 씀
-    for j in road[index]:
-            if not visited[j]:
-                visited[j]=1
-                dfs(j,num+1)
+    for j in road[index]: #여기서 오류가 나는데 아무래도 A 좀비 영역과 B 좀비 영역이 겹칠 때가 문제가 생기는 것 같다..
+        visited[j]=1
+        dfs(j,num+1)
+
 dp=[float('inf') for _ in range(N+1)]
 dp[1]=0
 def dijkstra():
@@ -32,13 +39,12 @@ def dijkstra():
             if nextcost<dp[i]:
                 dp[i]=nextcost
                 heapq.heappush(q,[nextcost,i])
-    return dp
+    if visited[N]: return dp[N]-Q
+    else: return dp[N]-P
 visited=[0]*(N+1)
 for i in zombie: 
     dfs(i,0)
-dijkstra()
-print(dp)
-
+print(dijkstra())
 
 #다익스트라로 위험구간 표시 + dfs
 #다익스트라로 1에서 N으로 가는 숙박비 표현 (DP)
