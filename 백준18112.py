77b01cc351d177a7d8fb7ba6b0254b68af455e80
20wjsdudtj@gmail.com <20wjsdudtj@gmail.com>
2022-04-13 14:43:49 +0900

    dfs깊이 오류

--- 백준18112.py
+++ 백준18112.py
@@ -2,11 +2,12 @@ start=input()
 end=input()
 
 def dfs(num, sum):
-    if num==int(end): return sum
+    if int(num,2)==end: return sum
     dfs(bin(int(num,2)+1),sum+1)
     dfs(bin(int(num,2)-1),sum+1)
     if int(num,2)!=0: 
-
+        for i in range(len(bin(num))):
+            dfs(num^(1<<i),sum+1)
 
 sum=0
-dfs(int(start), sum)
+dfs(start, sum)
