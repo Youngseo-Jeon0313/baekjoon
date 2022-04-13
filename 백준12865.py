5cf877850d42faa0b7f856a4275cb23a2e3172c3
20wjsdudtj@gmail.com <20wjsdudtj@gmail.com>
2022-04-13 14:15:01 +0900

    배낭문제(dptable)

---
+++ 백준12865.py
@@ -0,0 +1,12 @@
+
+n, k = [int(x) for x in input().split()]
+table = [0] * (k+1)
+for _ in range(n):
+    w, v = [int(x) for x in input().split()]
+    if w > k:
+      continue
+    for j in range(k, 0, -1):
+      if j + w <= k and table[j] != 0:
+        table[j+w] = max(table[j+w], table[j] + v)
+    table[w] = max(table[w], v)
+  print(max(table))
