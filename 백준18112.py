09c5a9fd64d5a24811330cefdacdd8f5bc37955a
20wjsdudtj@gmail.com <20wjsdudtj@gmail.com>
2022-04-24 12:17:32 +0900

    비트연산자 시간초과ㅠㅠㅠ

--- 백준18112.py
+++ 백준18112.py
@@ -1,19 +1,17 @@
-start=int(input())
-end=input()
+import sys
+input=sys.stdin.readline
+start=input().strip()
+end=input().strip()
 from collections import deque
-q=deque
+q=deque([])
 def dfs(q):
     while q:
         num,sum=q.popleft()
-        if str(bin(num)[2:])==end: print(sum); break; return;
+        if str(bin(num)[2:])==end: print(sum); break;
+        a=len(bin(num)[2:])
+        for i in range(a-1):
+            q.append([num^(1<<i),sum+1])
         q.append([num+1,sum+1])
-        q.append([num-1,sum+1])
-        if num!=0 :
-            a=len(bin(num)[2:])
-            for i in range(a+1):
-                q.append([num^(1<<i),sum+1])
-        print(q)
-sum=0
-q=deque([])
-q.append([start,sum])
+        if num!=0: q.append([num-1,sum+1])
+q.append([int(start,2),0])
 dfs(q)
