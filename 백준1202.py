874c67434f101e57555f34e162014d87eb101b11
20wjsdudtj@gmail.com <20wjsdudtj@gmail.com>
2022-05-10 14:36:05 +0900

    heapq 쓰기

--- 백준1202.py
+++ 백준1202.py
@@ -1,10 +1,22 @@
 '''
-모든 조건들이 난잡해보이지만
-Greedy임을 암시하고 있음
+거꾸로 다시 생각
+반례
 '''
-
+from heapq import *
 N,K=map(int,input().split())
-List=[]
-for _ in range(N):List.append(list(map(int,input().split())))
+List=[]; List2=[];
+for _ in range(N):
+    a,b=map(int,input().split())
+    heappush(List,[a,b])
 Bag=[int(input()) for _ in range(K)]
-
+Bag.sort()
+sum=0
+for i in range(K):  
+    flag=False
+    while len(List)  :
+        if Bag[i]<List[0][0] : break
+        x,y=heappop(List)
+        heappush(List2,-y)
+    if List2:
+        sum-=heappop(List2)
+print(sum)
