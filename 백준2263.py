1eb0756f5149704d7a51b223384e61bb475be210
20wjsdudtj@gmail.com <20wjsdudtj@gmail.com>
2022-06-03 12:35:30 +0900

    인오더와 포스트오더로 주어진 것을 분할정복을 통해 프리오더로 표현하기 (분할정복!!)

--- 백준2263.py
+++ 백준2263.py
@@ -1,44 +1,56 @@
 '''
-이 문제에서 내가 간과한 거는 기본적으로 여기서
-그 크기순서대로 들어가는 게 아니다ㅠㅠㅠ
+너무 똑똑하다...
+어떻게 이런 문제를 만들어냈지..????
+
+분할정복!!!!
+'''
+
+# num=1
+# N=int(input())
+# inorder=list(map(int,input().split()))
+# postorder=list(map(int,input().split()))
+# where=[-1]*(N+1) #inorder 속 숫자들은 어차피 다 다르다. 그들이 있는 위치를 [숫자]=어디 로 표시해줄 수단
+# for i in range(N):
+#     where[inorder[i]]=i
+
+# def preorder(left_root,right_root,root):
+    
+#     print(postorder[root-1]) #3(postorder[3-1]) #1(postorder[1-1] #2(postorder[2-1])
+#     #그 안에서 왼쪽 root로 가기
+#     if left_root-1==-1 or where[left_root]==-1: return
+#     preorder(where[left_root],left_root-1,left_root) # 0  0  1 # -1 -1  0
+#     #그 안에서 오른쪽 root로 가기
+
+#     preorder(where[right_root],right_root-1,right_root) #  1 1 2 # 0 0 1
+    
+    
+
+# preorder(where[N-1],N-1,N) # 1 2 3
 
 
 '''
 
+8
+2 1 4 3 5 7 6 8
+2 1 3 4 7 8 6 5
+'''
 
-class Tree:
-    def __init__(self,val=None) : #만약 Tree(30)이렇게 해주면 val에 자동으로 들어간다.
-        if val!=None: self.val=val
-        else: self.val = None
-        self.left = None
-        self.right = None
-
-    def insert(self, val): #기존 트리에 노드 삽입
-        if self.val :
-            if val < self.val:
-                if self.left is None: #밑으로 더 내려가
-                    self.left = Tree(val)
-                else:
-                    self.left.insert(val)
-            elif val > self.val:
-                if self.right is None:
-                    self.right = Tree(val)
-                else:
-                    self.right.insert(val)
-        else: self.val=val #root노드에 그냥 넣어준다.
-
-    def printValues(self): #왼쪽에서 중앙, 마지막으로 오른쪽에서 값을 인쇄하는 함수
-        print(self.val)
-        
-        if self.left :
-            self.left.printValues()
-        if self.right:
-            self.right.printValues()
-
+import sys
+sys.setrecursionlimit(10**6)
 N=int(input())
-in_order=list(map(int,input().split()))
-post_order=list(map(int,input().split()))
-tree=Tree()
-for i in post_order:
-    tree.insert(i)
-tree.printValues()
+inorder=list(map(int,input().split()))
+postorder=list(map(int,input().split()))
+pos=[0]*(N+1)
+for i in range(N):
+    pos[inorder[i]]=i
+
+def preorder(instart,inend,poststart,postend):
+    if instart>inend or poststart>postend: return
+    root=postorder[postend]
+    print(root,end=" ")
+    left=pos[root]-instart #왼쪽 인자 개수 #4개 #3개
+    right=inend-pos[root] #오른쪽 인자 개수 #4개
+    preorder(instart,instart+left-1,poststart,poststart+left-1) #0 3 0 3
+    preorder(inend-right+1,inend,postend-right,postend-1)#4 7 4 7
+
+preorder(0,N-1,0,N-1)
