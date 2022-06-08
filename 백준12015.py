67e8ffb7eba2e855247c51b519b6a7be3f12b09f
20wjsdudtj@gmail.com <20wjsdudtj@gmail.com>
2022-06-08 20:20:56 +0900

    LIS 빠르게 풀기

--- 백준12015.py
+++ 백준12015.py
@@ -1,20 +1,28 @@
+'''
+기존의 LIS는 for문 두 개로 해결해서 O(N^2)으로 해결할 수 있었으나
+이 문제는 O(logN)으로 해결하는 방법을 요구
+'''
+'''
+핵심은  '어차피!'
+
+어차피 그걸로 대체해도 상관없어!
+'''
 import sys
 input=sys.stdin.readline
-n=int(input())
-List=[]
 
-for _ in range(n):
-    x,y=map(int,input().split())
-    List.append([x,y])
-List.sort(key=lambda x:x[0])
+N=int(input())
+L=list(map(int,input().split()))
+List=[0]
+for i in L:
+    if List[-1]<i:List.append(i)
+    else:
+        #포인터 방식으로 적절한 위치 찾아가자!
+        left=0; right=len(List);
+        #이진탐색
+        while left<right:
+            mid=(left+right)//2
+            if List[mid]<i:left=mid+1
+            else: right=mid
+        List[right]=i
 
-dp=[1]*1000
-for i in range(n):
-    Max=0
-    for j in range(i):
-        #증가하기만 하면 그냥 일단 넣기
-        if List[i][1]>List[j][1] :
-            Max=max(Max, dp[List[j][0]])
-    dp[List[i][0]]+=Max
-
-print(n-max(dp))
+print(len(List)-1)
