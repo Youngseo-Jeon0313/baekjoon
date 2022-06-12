5b793731d06fc4c68c895483b37ca3d9aea352b6
20wjsdudtj@gmail.com <20wjsdudtj@gmail.com>
2022-06-12 09:23:24 +0900

    위상정렬

---
+++ check.py
@@ -0,0 +1,7 @@
+def spam(eggs):
+    eggs.append(1)
+    eggs=[2,3]
+    
+ham=[0]
+spam(ham)
+print(ham)
--- 백준13460.py
+++ 백준13460.py
@@ -5,17 +5,29 @@
 import sys
 from copy import deepcopy
 
-
-
 N,M=map(int,input().split()) #세로 가로
 List=[]
 for _ in range(N):List.append(list(input()))
 
-
+flag=False
 def dfs(board,cnt):
-    global ans
-    if cnt==5:
-        for i in range(N):
-            for j in range(M):
+    global flag
+    for i in range(N):
+            if 'R' in List[i]: flag=True
+            if 'B' in List[i]: flag=False; break
+    if flag==False and cnt==10: print(-1); return 
+    elif flag==True: print(cnt); return
+    for i in range(1,5):
+        copyboard=move(deepcopy(board),i)
+        dfs(deepcopy(copyboard),cnt+1)
 
+def move(board,how):
+    if how==1: #왼쪽으로 쫙
+        for i in range(N,-1,-1):
+            if board[i]!='R':
+                while 
 
+    elif how==2: #오른쪽으로 쫙
+    elif how==3: #아래로 쫙
+    elif how==4: #위로 쫙
+    return board
