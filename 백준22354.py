fe697833a1b13ea76c9f0986b15baa003978370d
20wjsdudtj@gmail.com <20wjsdudtj@gmail.com>
2022-06-30 19:12:54 +0900

    greedy, sort (이 때 NlogN을 넘어가지 않도록 한다)

--- 백준22354.py
+++ 백준22354.py
@@ -6,39 +6,48 @@ arr=list(input())
 score=list(map(int,input().strip().split()))
 
 
-ans=0
 q=deque([])
 for i in range(n):
     q.append((i,arr[i]))
 
-q_=deque([])
-q_.append(q.popleft())
-while q:
-    index,color=q.popleft()
-    if q_[-1][1]==color : 
-        #만약에 내가 뽑은 거가 똑같은 색인데 더 크면
-        if score[q_[-1][0]]<score[index] or index==n-1 or q_[-1][0]!=0:
-            q_.pop()
-            q_.append((index,color))
-    else:
-        q_.append((index,color))
 
-N=len(q_)
+def greedy(q):
+    q_=deque([])
+    while q:
+        index,color=q.popleft()
+        if len(q_)>0 and q_[-1][1]==color : 
+            #만약에 내가 뽑은 거가 똑같은 색인데 더 크면
+            if score[q_[-1][0]]<score[index] :
+                q_.pop()
+                q_.append((index,color))
+        else:
+            q_.append((index,color))
+    return q_
+
 
 ans=0
 ansList=[]
-q=sorted(q_, key = lambda x: -score[x[0]])
+
 q=deque(q)
+q=greedy(q)
+if len(q)<=2: print(0); exit();
+q.popleft(); q.pop();
+q=sorted(q, key = lambda x: (-score[x[0]]))
 
-popthing,Color=q.popleft()
-ansList.append(popthing)
-ans+=score[popthing]
+N=len(q)
 
-while len(ansList)<(N-2)//2+N%2 and q:
-    popthing, Color = q.popleft()
-    if popthing != 0 and popthing !=n-1 and popthing+1 not in ansList and popthing-1 not in ansList:
-        #여기서 시간초과를 당하는 것 같다ㅠㅠㅠㅠ
-        ansList.append(popthing)
-        ans+=score[popthing]
+
+for i in range(N//2+N%2):
+    ans+=score[q[i][0]]
 print(ans)
-            
+'''
+
+3(검) 1(흰) 2(검) 10(흰) 9(검) 3(흰) 4(검)
+10를 꺼내면서 2,9를 지우지 않아도 2를 꺼내면서 점수를 얻지 못하고, 9를 꺼낼 수도 있어요!
+혹시 이런 식으로 짠걸수도?..?.??..
+
+<반례> 
+3
+WBW
+20 1 20
+'''
