b6da2b052391ddb1abee5e629039c82defcefab0
20wjsdudtj@gmail.com <20wjsdudtj@gmail.com>
2022-07-07 20:45:45 +0900

    Floodfill

--- UCPCF.py
+++ UCPCF.py
@@ -1,49 +1,57 @@
+from collections import deque
+
 C,L,leng=map(int,input().split())
-str_arr=[]
 road=[]
-dic={}
-
-for i in range(C):
-    str_arr.append(input())
-for i in range(C):
-    for j in range(L):
-            if str_arr[i][j] in dic.keys():
-                dic[str_arr[i][j]].append([i,j])
-            else:
-                dic[str_arr[i][j]]=[[i,j]]
-D,U,L,R=[1,0,0,0],[-1,0,0,0],[0,0,-1,0],[0,0,0,1]
+for _ in range(C): road.append(input())
+dx,dy=[1,-1,0,0],[0,0,1,-1]
 charac=input()
-def rec(x,y,ans,string):
-    if str_arr[x][y]==ans:
-        return
-    else:
-        rec(x+1,y,ans,string+'D')
-        rec(x-1,y,ans,string+'U')
-        rec(x,y+1,ans,string+'R')
-        rec(x,y-1,ans,string+'L')
-
-totalans=[];
+check=[[0 for _ in range(L)] for _ in range(C)] 
+charac+='0'
 
 sum=0;
 before_x=0; before_y=0; flag=True;
-charac=charac+'0'
-while True:
+totalans=[];
+q=deque()
+q.append([0,0])
+
+while True: #반복해서 charac이 있는지를 확인할 거다.
+    #여기서부터는 이 단어 속에서 한 바퀴 돌 것을 생각해서 가져올 것임
     ans=[]
-    for i in range(len(charac)):
-        if charac[i]=='0':
-            x,y=C-1,L-1
-            if x>before_x: ans.append('D'*(x-before_x)); before_x=x
-            if x<before_x: ans.append('U'*(before_x-x)); before_x=x;
-            if y>before_y: ans.append('R'*(y-before_y)); before_y=y;
-            if y<before_y: ans.append('L'*(before_y-y)); before_y=y;
-        elif (charac[i] in dic.keys()) and len(list(dic[charac[i]]))>0  :
-            x,y=dic[charac[i]].pop()
-            before_x,before_y,ch,answer=rec(before_x,before_y,charac[i],'')
-            ans.append(answer)
-            ans.append('P')
-        else: flag=False; break;
-    if flag==False: break; 
-    if flag==True: sum+=1; totalans.extend(ans)
+    flag=False #flag의 핵심은 '한 번이라도' 걸리면 break로 탈출시켜 버릴 것!!
+    for i in charac:
+        check_=[[0 for _ in range(L)] for _ in range(C)]
+        flag_=False;
+        if i=='0': flag_=True; 
+        if flag_==False:
+            while q:
+                #그 단어 한 개를 현재 위치에서 최소 비용을 들여 찾을 것
+                x,y=q.popleft()
+                if road[x][y]==i and not check[x][y]: check[x][y]=True; flag_=True; break;
+                else:
+                    for j in range(4):
+                        nx=x+dx[j]; ny=y+dy[j]
+                        if 0<=nx and nx<C and 0<=ny and ny<L and not check_[nx][ny] : q.append([nx,ny]); check_[nx][ny]=1
+
+        if flag_==True:
+            if i!='0':
+                if x>before_x: ans.append('D'*(x-before_x)); before_x=x
+                if x<before_x: ans.append('U'*(before_x-x)); before_x=x;
+                if y>before_y: ans.append('R'*(y-before_y)); before_y=y;
+                if y<before_y: ans.append('L'*(before_y-y)); before_y=y;
+                ans.append('P')
+            if i=='0':
+                x,y=C-1,L-1
+                if x>before_x: ans.append('D'*(x-before_x)); before_x=x
+                if x<before_x: ans.append('U'*(before_x-x)); before_x=x;
+                if y>before_y: ans.append('R'*(y-before_y)); before_y=y;
+                if y<before_y: ans.append('L'*(before_y-y)); before_y=y;
+                flag=True
+
+        else: break;           
+    if flag==True:
+        sum+=1; totalans.extend(ans)
+    else: break;
+    
 ans=''
 for i in totalans:
     ans+=i
--- check.py
+++ check.py
@@ -1,7 +1,2 @@
-def issquare(a,b):
-    n=a*b
-    sqrt = n ** (1/2)
-    if sqrt % 1 ==0: return True
-    else: return False
-
-print(issquare(10**3, 139282434))
+for _ in range(50):
+    print('a'*50)
