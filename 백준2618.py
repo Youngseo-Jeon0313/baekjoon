1d0888794d100338e57c0356a2c9519b7b46f232
20wjsdudtj@gmail.com <20wjsdudtj@gmail.com>
2022-07-25 22:12:47 +0900

    DP 경찰차문제

--- 백준2618.py
+++ 백준2618.py
@@ -1,5 +1,5 @@
-from re import L
-
+import sys
+input=sys.stdin.readline
 
 N=int(input())
 W=int(input())
@@ -11,55 +11,55 @@ for _ in range(W):
     position.append([i-1,j-1])
     position1.append([i-1,j-1])
 dp=[[float('inf') for _ in range(W+1)] for _ in range(W+1)]
-
+dp_trace=[[0 for _ in range(W+1)] for _ in range(W+1)] #어디에서 왔니
 #print(position)
 dp[0][0]=0
 ans=[]
-def backtracking(i,j):
-    if i==0 and j==0: return
-    if j!=1 and j-1==i:
-        for k in range(j-1):
-            if dp[i][k]==dp[i][j]-(abs(position1[j][0]-position1[k][0])+abs(position1[j][1]-position1[k][1])):
-                ans.append(2)
-                backtracking(i,k)
-    elif i!=1 and i-1==j:
-        for k in range(i-1):
-            if dp[k][j]==dp[i][j]-(abs(position[i][0]-position[k][0])+abs(position[i][1]-position[k][1])):
-                ans.append(1)
-                backtracking(k,j)
-    elif i<j:
-        ans.append(2)
-        backtracking(i,j-1)
-    elif i>j:
-        ans.append(1)
-        backtracking(i-1,j)
 
+#O(W^3)
 for i in range(W+1):
     for j in range(W+1):
         if i==j: continue
         if i<j:
-            if j!=1 and j-1==i:
+            if i and j-1==i:
                 for k in range(j-1):
-                    dp[i][j]=min(dp[i][j],dp[i][k]+abs(position1[j][0]-position1[k][0])+abs(position1[j][1]-position1[k][1]))
+                    if dp[i][j]>dp[i][k]+abs(position1[j][0]-position1[k][0])+abs(position1[j][1]-position1[k][1]):
+                        dp[i][j]=dp[i][k]+abs(position1[j][0]-position1[k][0])+abs(position1[j][1]-position1[k][1])
+                        dp_trace[i][j]=k
             else:
-                 dp[i][j]=dp[i][j-1]+abs(position1[j][0]-position1[j-1][0])+abs(position1[j][1]-position1[j-1][1])
+                dp[i][j]=dp[i][j-1]+abs(position1[j][0]-position1[j-1][0])+abs(position1[j][1]-position1[j-1][1])
+                dp_trace[i][j]=j-1
         else:
-            if i!=1 and i-1==j:
+            if j and i-1==j:
                 for k in range(i-1):
-                    dp[i][j]=min(dp[i][j],dp[k][j]+abs(position[i][0]-position[k][0])+abs(position[i][1]-position[k][1]))
-          
+                    if dp[i][j]>dp[k][j]+abs(position[i][0]-position[k][0])+abs(position[i][1]-position[k][1]):
+                        dp[i][j]=dp[k][j]+abs(position[i][0]-position[k][0])+abs(position[i][1]-position[k][1])
+                        dp_trace[i][j]=k
             else:
                 dp[i][j]=dp[i-1][j]+abs(position[i][0]-position[i-1][0])+abs(position[i][1]-position[i-1][1])
+                dp_trace[i][j]=i-1
+# for i in dp:
+#     print(*i)
+compare=float('inf')
+I,J=0,0
+#O(W^2)
+for i in range(W):
+    if dp[i][W]<compare: I,J = i,W; compare=dp[i][W]
+    if dp[W][i]<compare: I,J = W,i; compare= dp[W][i]
+# for j in range(W):
+#     if dp[W][j]<compare: I,J = W, j; compare= dp[W][j]
 
-print(position)
-print(min(dp[W-1][W],dp[W][W-1]))
-if dp[W-1][W]<=dp[W][W-1]: backtracking(W-1,W)
-else: backtracking(W,W-1)
-
-for i in dp:
-    print(*i)
-
-for i in range(len(ans)-1,-1,-1):
+#    if dp[W][i]<compare: I,J = W, i; compare= dp[W][i]
+# print('I랑 J',I,J)
+# print(dp_trace)
+print(dp[I][J])
+for i in range(W):
+    if J>I:
+        J=dp_trace[I][J]
+        ans.append(2)
+    else:
+        I=dp_trace[I][J]
+        ans.append(1)
+for i in range(W-1,-1,-1):
     print(ans[i])
 #역추적??
-
