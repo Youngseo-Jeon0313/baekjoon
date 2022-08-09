241c99fdfb0257b9e6706bfe335fd2daa303d921
20wjsdudtj@gmail.com <20wjsdudtj@gmail.com>
2022-08-09 14:52:25 +0900

    배낭문제

--- 백준23741.py
+++ 백준23741.py
@@ -3,24 +3,25 @@ input=sys.stdin.readline
 from collections import deque
 
 N,M,X,Y=map(int,input().split())
-List=[[] for _ in range(N+1)] #뭐가 뭐랑 연결되어있는지 체크
+List=[[] for _ in range(N+1)]
+visit=[[0 for _ in range(Y+1)] for _ in range(N+1)] #pos,visit를 방문표시할 visit함수
 for _ in range(M):
-    x,y=map(int,input().split())
-    List[x].append(y)
-    List[y].append(x)
+  x,y=map(int,input().split())
+  List[x-1].append(y-1)
+  List[y-1].append(x-1)
+
+deq=deque([])
+deq.append([X-1,0])
+visit[X-1][0]=1
 ans=[]
-'''
-인자는 이렇게 한다.
--depth
--현재 정점의 위치
--그걸 담는 arr
-'''
-
-deque=([])
-
-
-
-if len(ans)>0:
-    print(list(set(ans)))
-else:
-    print(-1)
+while deq:
+  pos,depth=deq.popleft()
+  if depth==Y and pos+1 not in ans: ans.append(pos+1) #%2==Y%2
+  if depth<Y:
+      for i in List[pos]:
+          if not visit[i][depth+1]:
+            deq.append([i,depth+1])
+            visit[i][depth+1]=1
+ans.sort()
+if len(ans)==0: print(-1)
+else: print(*ans)
