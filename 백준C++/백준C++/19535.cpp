f642db45e91b8d76abeb2a9034eb203dc6ceda37
20wjsdudtj@gmail.com <20wjsdudtj@gmail.com>
2022-08-13 22:56:06 +0900

    C++로 구현하기

---
+++ 백준C++/백준C++/19535.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int>v;
+
+
+int main() {
+	std::ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	int N;
+	cin >> N;
+	v.resize(N);
+
+	for (int i = 1; i <= N: i++) {
+		cin >> v[i] >> ' ';
+	}
+
+
+	return 0;
+}
