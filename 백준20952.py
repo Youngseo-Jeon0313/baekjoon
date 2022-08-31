ebdb083cc9c0457bde65493d3549499fd391ac70
20wjsdudtj@gmail.com <20wjsdudtj@gmail.com>
2022-08-31 22:36:32 +0900

    시간초과 targeting / MOD주의 / 누적합 / [ ] arr 1,0으로 확인하기

--- 백준20952.py
+++ 백준20952.py
@@ -8,8 +8,7 @@ arr=[0,0,0,0,0,0,0] #0,1,2,3,4,5,6
 ans=0
 
 for i in A:
-    if arr[i%7]==0: ans+=1
-    arr[i%7]=1
+    if arr[i%7]==0: ans+=1; arr[i%7]=1
 
 
 finalB=0
@@ -17,17 +16,13 @@ for i in range(M):
     check=(7-(B[i]+finalB)%7)%7
     if arr[check]==1: #아직 안 지워진 값이라면
         if ans==1: continue; #다 지워지려고 하면 원상복귀
-        else: 
+        else: #연산 적용
             arr[check]=0; ans-=1; 
-            finalB+=B[i]#지워져야 하는 순간 ans-=1하고
-
-
-
+    finalB+=B[i]#지워져야 하는 순간 ans-=1하고
 
 ansarr=[]
 for i in range(N):
-    if arr[A[i]%7]==0: continue
-    ansarr.append((A[i]+finalB)%MOD)
+    if arr[A[i]%7]==1: ansarr.append((A[i]+finalB)%MOD)
 print(len(ansarr))
 print(*ansarr)
 
@@ -40,10 +35,4 @@ print(*ansarr)
 답
 2
 999999978 999999979
-
-
-
-10e+7로 나눌 때 주의하자ㅠㅠㅠㅠㅠㅠㅠㅠ
-
 '''
-
