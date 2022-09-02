fbd9b4cef4148c98e918b9ff9990761901c3ff68
20wjsdudtj@gmail.com <20wjsdudtj@gmail.com>
2022-09-02 23:42:15 +0900

    게임이론.그냥파악

--- 백준9655.py
+++ 백준9655.py
@@ -1,8 +1,5 @@
+#점화식 ㅠㅠㅠ 그냥 파악해야 함
+
 n=int(input())
-dp=[0]*(n+1)
-dp[1]=1; dp[2]=2; dp[3]=1
-for i in range(4, n+1):
-    if dp[i-1]==2 or dp[i-3]==2: dp[i]=1
-    else: dp[i]=2
-if dp[n]==1: print('SK')
-else: print('CY')
+if n%2==0: print('CY')
+else: print("SK")
