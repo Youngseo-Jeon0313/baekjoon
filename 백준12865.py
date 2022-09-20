0b08b4e8cddae1bafddbe16f047067eb801ec49a
20wjsdudtj@gmail.com <20wjsdudtj@gmail.com>
2022-09-20 23:59:45 +0900

    dp check

--- 백준12865.py
+++ 백준12865.py
@@ -1,6 +1,7 @@
 
 n, k = [int(x) for x in input().split()]
 table = [0] * (k+1)
+
 for _ in range(n):
     w, v = [int(x) for x in input().split()]
     if w > k:
@@ -9,4 +10,7 @@ for _ in range(n):
       if j + w <= k and table[j] != 0:
         table[j+w] = max(table[j+w], table[j] + v)
     table[w] = max(table[w], v)
+
+print(table)
+print(table[k])
 print(max(table))
