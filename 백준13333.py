6fc019dd3a28cf4658a0570fbe409fbe2557c8d6
20wjsdudtj@gmail.com <20wjsdudtj@gmail.com>
2022-09-30 13:41:17 +0900

    쉬운 구현

---
+++ 백준13333.py
@@ -0,0 +1,12 @@
+import sys
+input=sys.stdin.readline
+
+N=int(input())
+List=list(map(int,input().split()))
+List.sort()
+for j in range(N):
+    for i in range(10000):
+    
+        if List[j]>=i and N-i==j :
+            print(i); exit()
+print(0)
