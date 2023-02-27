7be40bebf9eb44a2d4cb6db4be6a8ddfc93135b2
Youngseo-Jeon0313 <20wjsdudtj@gmail.com>
2023-02-27 13:34:52 +0900

    bfs

--- PG가장먼노드.py
+++ PG가장먼노드.py
@@ -1,13 +1,25 @@
-#얘를 포함해 말아
-def solution(triangle):
-    DP=[[0 for _ in range(len(triangle))] for _ in range(len(triangle))]
-    DP[0][0]=triangle[0][0]
-    for i in range(len(triangle)-1):
-        for j in range(i+1):
-            DP[i+1][j]=max(DP[i+1][j],DP[i][j]+triangle[i+1][j])
-            DP[i+1][j+1]=max(DP[i+1][j+1], DP[i][j]+triangle[i+1][j+1])
-    # print(DP)
-    answer = max(DP[len(triangle)-1])
+from collections import deque
+
+def solution(n, edge):
+    depth_list=[0 for _ in range(n+1)]
+    adj=[[] for _ in range(n+1)]
+    answer=0
+    for i in edge:
+        a,b=i[0],i[1]
+        adj[a].append(b)
+        adj[b].append(a)
+    deq=deque([[1,1]])
+    depth_list[1]=1
+    while deq:
+        node, depth=deq.popleft()
+        for j in adj[node]:
+            if not depth_list[j]: 
+                depth_list[j]=depth+1
+                deq.append([j,depth+1])
+    # print(depth_list)                
+    for k in range(n+1):
+        if depth_list[k]==max(depth_list): answer+=1
+
     return answer
 
-print(solution([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]))
+print(solution(6, [[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]))
