a47b2e0b9abf5cf7ec3e38f7f4c85a39ba222e4c
Youngseo-Jeon0313 <20wjsdudtj@gmail.com>
2023-03-04 10:32:53 +0900

    진수변환

---
+++ 진수변환.py
@@ -0,0 +1,11 @@
+def solution(n, q):
+    rev_base = ''
+
+    while n > 0:
+        n, mod = divmod(n, q)
+        rev_base += str(mod)
+
+    return rev_base[::-1] 
+    # 역순인 진수를 뒤집어 줘야 원래 변환 하고자하는 base가 출력
+    
+print(solution(45, 3))
