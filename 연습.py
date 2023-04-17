9b6a264006694edcd63863096f17e23461884b60
Youngseo-Jeon0313 <20wjsdudtj@gmail.com>
2023-04-17 17:27:57 +0900

    CCW를 활용하여 선분 두 개가 겹치는지 확인하기

--- 연습.py
+++ 연습.py
@@ -1,2 +1,18 @@
-a=input("Your name?")
-print("Hello", a,'\n'"Hello",a)
+n = int(input())
+arr = list(map(int, input().split()))
+youngsik = 0
+minsik = 0
+for i in range(n):
+    #영식 요금제
+    x = arr[i] // 30
+    youngsik += 10 * x + 10
+    #민식 요금제
+    y = arr[i] // 60
+    minsik += 15 * y + 15
+
+if youngsik == minsik:
+    print('Y','M',minsik)
+elif youngsik > minsik: #민식 요금제가 더 쌈
+    print('M',minsik)
+else: #영식 요금제가 더 쌈
+    print('Y',youngsik)
