5787ab16b0434690d2328cab834a7707043976cb
youngseojeon <20wjsdudtj@gmail.com>
2023-05-31 17:28:13 +0900

    [Bronze III] Title: 베시와 데이지, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/16431. 베시와 데이지/베시와 데이지.py
@@ -0,0 +1,9 @@
+Br, Bc=map(int,input().split())
+Dr, Dc=map(int,input().split())
+Jr, Jc=map(int,input().split())
+
+B=max(abs(Jr-Br),abs(Jc-Bc))
+D=abs(Jr-Dr)+abs(Jc-Dc)
+if B<D: print('bessie')
+elif B>D: print('daisy')
+else: print('tie')
