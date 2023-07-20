c455bfceb145b93bfeba987941749a92c99a3aec
Youngseo-Jeon0313 <20wjsdudtj@gmail.com>
2023-07-21 07:39:07 +0900

    이분탐색

--- 이진탐색.py
+++ 이진탐색.py
@@ -46,3 +46,23 @@ def binary_search(target, data):
             end = mid -1
 
     return None
+
+
+#-----------------------------------------
+N=int(input())
+K=int(input())
+start = 1
+end = K
+
+while start <= end:
+    mid = (start + end) // 2
+    num = 0
+    for i in range(1,N+1):
+        num += min(mid // i, N)
+    if num >= K:
+        ans = mid ## 여기 주의!!
+        end = mid -1
+    else:
+        start = mid + 1
+
+print(ans)
