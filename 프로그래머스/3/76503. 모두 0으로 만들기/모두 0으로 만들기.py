6d5f4bba4da4ef844c610a34c6314a305bd2e9ae
youngseojeon <20wjsdudtj@gmail.com>
2025-03-19 15:08:19 +0900

    [level 3] Title: 모두 0으로 만들기, Time: 1091.55 ms, Memory: 322 MB -BaekjoonHub

--- 프로그래머스/3/76503. 모두 0으로 만들기/모두 0으로 만들기.py
+++ 프로그래머스/3/76503. 모두 0으로 만들기/모두 0으로 만들기.py
@@ -1,26 +1,36 @@
 import sys
 sys.setrecursionlimit(300000)
 
+'''
+끝에 도달하면 ? -> 자기를 0으로 만들기 위해 부모에게 자기를 더해야 함.
+그럼 각각의 노드는 무엇을 반환해야 해? -> 자신의 값을 반환
+정답은 그 반환하는 전체 횟수가 중요함 -> abs(더하거나 빼는 행위의 값)
+
+'''
+
 answer = 0
 def solution(a, edges):
-    if sum(a) != 0: return -1
-    
-    visited=[False for _ in range(len(a))]
-    
+    global answer
+    if sum(a)!=0: return -1
     adj = [[] for _ in range(len(a))]
-    for start,end in edges:
-        adj[start].append(end)
-        adj[end].append(start)
+    for i in edges:
+        adj[i[0]].append(i[1])
+        adj[i[1]].append(i[0])
+    visited = [0 for _ in range(len(a))]
     
     def dfs(node):
         global answer
-        visited[node]=True
-        for adj_node in adj[node]:
-            if not visited[adj_node]:
-                visited[adj_node]=True
-                dfs(adj_node)
-                a[node]+=a[adj_node]
-                answer+=abs(a[adj_node])
-    dfs(0)
+        visited[node]=1
+        temp = 0
+        for i in adj[node]:
+            if not visited[i]:   
+                check = dfs(i)
+                a[node]+=check
+                answer+=abs(check)
+                #print(check)
+                
+        return a[node]
     
+    #print(a)
+    dfs(0)
     return answer
